424520300377f3a8417f45bc8b874e801a2cfc83
s-prak <sprakash_cse210557@mgit.ac.in>
2025-05-13 16:50:36 +0530

    correct path

--- src/individual-repo/npm/overall.sh
+++ src/individual-repo/npm/overall.sh
@@ -11,7 +11,7 @@ mkdir -p ./tmp/result-individual
 npx cyclonedx-npm --ignore-npm-errors --output-format "XML" --output-file "./tmp/result-individual/SBOM.xml"
 
 #Convert SBOM into csv fornat
-xsltproc tmp/ml-depcheck-utility/src/individual-repo-npm/components.xslt "./tmp/result-individual/SBOM.xml" > "./tmp/result-individual/SBOM.csv"
+xsltproc tmp/ml-depcheck-utility/src/individual-repo/npm/components.xslt "./tmp/result-individual/SBOM.xml" > "./tmp/result-individual/SBOM.csv"
 
 #Append fields to sbom - deprecated and last publish
 node tmp/ml-depcheck-utility/src/individual-repo/npm/append-fields.js
